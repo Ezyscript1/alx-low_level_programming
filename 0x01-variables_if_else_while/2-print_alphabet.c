efb9b319062e69d8cece9dfedce85d654f0f2152
Ezyscript1 <adegokeisreal48@gmail.com>
2023-02-23 07:54:18 -0800

    Alphabeth

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
+/* main - entry point */
+/* Return: always zero */
 int main(void)
 {
 	char lowerAlpha;
 
 	for (lowerAlpha = 'a'; lowerAlpha <= 'z'; lowerAlpha++)
 	{
-		putchar( lowerAlpha);
+		putchar(lowerAlpha);
 	}
+	putchar("\n");
 
 	return (0);
 }
