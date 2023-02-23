e98dca9ec3dab0ada30a903cd96f24d03a2ceaf7
Ezyscript1 <adegokeisreal48@gmail.com>
2023-02-23 10:15:56 -0800

    Omiting letter

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,10 @@ int main(void)
 	char letter = 'a';
 	while(letter <	'z')
 	{
-		putchar(letter);
 		if (letter == 'q' || 'e')
 		break;
 		letter++;
+		putchar(letter);
 	}
 	putchar('\n');
 	return (0);
