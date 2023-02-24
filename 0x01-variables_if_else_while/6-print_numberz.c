d4299b2eeec636199c63f7f48bfe53226cf43926
Ezyscript1 <adegokeisreal48@gmail.com>
2023-02-24 07:52:06 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return 0
+ */
+
+int main(void)
+
+{
+	int num;
+
+	for(num = 0; num <= 9; num++)
+	{
+		putchar('0' + (num % 10));
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
