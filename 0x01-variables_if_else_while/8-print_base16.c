653696f97277899443346e6356fe38f24a7dd1e4
Ezyscript1 <adegokeisreal48@gmail.com>
2023-02-24 08:35:32 -0800

    Hexadecimal shit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	int letter;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + (num % 10));
+	}	
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
