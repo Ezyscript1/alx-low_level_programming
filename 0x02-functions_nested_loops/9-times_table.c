276a627732f78c5c53c40b6b655fb410742fa975
Ezyscript1 <adegokeisreal48@gmail.com>
2023-03-01 07:56:57 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table from 0 - 9.
+ * Return: Nothing.
+*/
+
+void times_table(void)
+
+{
+	int i, j, ret;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			ret = i * j;
+			if (j > 0)
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+			else if (ret >= 10)
+			{
+				_putchar((ret / 10) + 'o');
+				_putchar((ret % 10) + '0');
+			}
+			else if (ret < 10 && j > 0)
+			{
+				_putchar(' ');
+			}
+		}
+	}
+}
