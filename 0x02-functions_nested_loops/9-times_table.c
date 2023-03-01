a4ac7794c185505bc20f1a6fc6df76df93b75aed
Ezyscript1 <adegokeisreal48@gmail.com>
2023-03-01 10:26:36 -0800

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar((ret % 10) + '0');
 			}
-			else if (ret > 0)
+			else if (ret > 10)
 			{
 			_putchar((ret / 10) + '0');
 			_putchar((ret % 10) + '0');
