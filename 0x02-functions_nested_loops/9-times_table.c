b8ec600a6197bcd0f6732909fa6ec20331bece4d
Ezyscript1 <adegokeisreal48@gmail.com>
2023-03-01 10:14:07 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,11 @@
 void times_table(void)
 {
 	int i, j, ret;
-	for (i = 0; i < 10; i ++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			ret = (i * j);
+			ret = i * j;
 			if (j > 0)
 			{
 				_putchar(',');
@@ -27,6 +27,10 @@ void times_table(void)
 			{
 			_putchar((ret / 10) + '0');
 			_putchar((ret % 10) + '0');
+			}
+			else
+			{
+				_putchar((ret % 10) + '0');
 			}
 		}
 		_putchar('\n');
