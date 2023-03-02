a494612001902c8af5ea4b21e1dffb859cca476d
Ezyscript1 <adegokeisreal48@gmail.com>
2023-03-02 04:48:08 -0800

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - print times table of any input.
+ * Return: Nothing
+ */
+
+void print_times_table(int n)
+{
+	int i, j, ret;
+	if ((n < 15) && (n > 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				ret = i * j;
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (ret >= 10)
+				{
+					_putchar((ret / 10) + '0');
+					_putchar((ret % 10) + '0');
+				}
+				else if (ret < 10)
+				{
+					_putchar(' ');
+					_putchar((ret % 10) + '0');
+				}
+				else
+				{
+					_putchar((ret % 10) + '0');
+				}
+			}
+			_putchar(10);
+		}
+	}
+}
