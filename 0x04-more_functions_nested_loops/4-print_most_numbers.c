eab91742c85db1d6c18b7625d9388ebd8fb55a3a
Ezyscript1 <adegokeisreal48@gmail.com>
2023-03-05 05:36:36 -0800

    Numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,11 @@
 void print_most_numbers(void)
 
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i <= 9)
 	{
-		if ((i != 4) && (i != 2))
+		if ((i != 2) && (i != 4))
 		{
 			_putchar(i);
 		}
