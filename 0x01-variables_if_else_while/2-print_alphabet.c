e7cea7aee7ac1b3832b8f69204a9dba8a355ad59
Ezyscripts <adegokeisreal48@gmail.com>
2023-05-21 17:24:09 -0700

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char let = 'a';
+
+	while (let < 'z')
+	{
+		putchar(let);
+		let++;
+	}
+	putchar(10);
+
+	return (0);
+}
