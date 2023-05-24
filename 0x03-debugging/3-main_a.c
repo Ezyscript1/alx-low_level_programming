e9d43d6fe0dbdd4d3e4c14ce22048a0c0c940fa3
Ezyscripts <adegokeisreal48@gmail.com>
2023-05-23 19:17:42 -0700

    .

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -12,7 +12,7 @@ int main(void)
 	int month;
 	int day;
 	int year;
-	
+
 	month = 4;
 	day = 01;
 	year = 1997;
@@ -20,8 +20,7 @@ int main(void)
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
 	day = convert_day(month, day);
-
-    	print_remaining_days(month, day, year);
+	print_remaining_days(month, day, year);
 
 	return (0);
 }
